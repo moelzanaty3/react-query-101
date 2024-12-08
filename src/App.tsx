b4e4ccd59e712be70ab024601ddcb4dc0de32fba
moelzanaty3 <mohammedelzanaty129@gmail.com>
2024-12-08 11:53:31 +0200

    ✨ feat: enhance repository fetching with search functionality, improve error handling, and update UI layout. Add search input and refresh option for stale data.

--- src/App.tsx
+++ src/App.tsx
@@ -12,42 +12,50 @@ interface Repository {
 	updated_at: string
 }
 
-const fetchRepositories = async (sort: string) => {
+const fetchRepositories = async (query: string, sort: string) => {
 	const url = new URL('https://api.github.com/search/repositories')
 
 	// Search for popular Facebook repositories as an example
-	url.searchParams.set('q', '@facebook')
+	url.searchParams.set('q', query)
 	// Only set sort if it's not the default (best match)
 	sort && url.searchParams.set('sort', sort)
 
 	const response = await fetch(url)
 
 	if (!response.ok) {
-		throw new Error('Failed to fetch repositories')
+		const errorData = await response.json()
+		if (errorData.errors) {
+			throw new Error(errorData.errors[0].message)
+		} else {
+			throw new Error('Failed to fetch repositories')
+		}
 	}
 
 	const data = await response.json()
 	return data.items as Repository[]
 }
 
-const useRepositoryList = (sort: string) => {
+const useRepositoryList = (query: string, sort: string) => {
 	return useQuery<Repository[]>({
-		queryKey: ['repositories', sort],
-		queryFn: () => fetchRepositories(sort),
-		staleTime: 5 * 60 * 1000, // 5 minutes
+		queryKey: ['repositories', query, sort],
+		queryFn: () => fetchRepositories(query, sort),
+		staleTime: 1 * 60 * 1000, // 1 minute
+		gcTime: 30 * 1000, // 30 seconds
+		enabled: !!query,
 	})
 }
 
 function App() {
 	const [sort, setSort] = useState<string>('')
-	const { data, isLoading, isError } = useRepositoryList(sort)
-
+	const [query, setQuery] = useState<string>('')
+	const { data, isLoading, isError, isStale, refetch, error } =
+		useRepositoryList(query, sort)
 	return (
 		<div className="container">
 			<div className="header">
 				<h1>GitHub Repositories</h1>
 				<select
-					disabled={isLoading}
+					disabled={isLoading || !data}
 					value={sort}
 					onChange={(e) => setSort(e.target.value)}
 					className="sort-select"
@@ -58,12 +66,31 @@ function App() {
 					<option value="updated">Recently updated</option>
 				</select>
 			</div>
+			<form className="search-form">
+				<input
+					type="text"
+					name="query"
+					placeholder="Search Keywords"
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+			</form>
 
 			{isLoading && <div className="loading">Loading repositories...</div>}
 
-			{isError && <div className="error">Error fetching repositories!</div>}
+			{isError && <div className="error">{error?.message}</div>}
 
 			<div className="repo-list">
+				{isStale && !isLoading && !isError && (
+					<p className="message">
+						Repositories data may be outdated. Click{' '}
+						<button className="link" onClick={() => refetch()}>
+							Refresh
+						</button>{' '}
+						to get the latest updates.
+					</p>
+				)}
+
 				{data?.map((repo) => (
 					<div key={repo.id} className="repo-item">
 						<div className="repo-header">
